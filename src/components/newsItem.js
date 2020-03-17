8bc54b3fd871a57966f79124576ea3fa65589f2b
Shreyas Prakash <shreyas314159@gmail.com>
2020-03-17 23:29:27 +0100

    initial commit

--- src/components/newsItem.js
+++ src/components/newsItem.js
@@ -1,12 +1,24 @@
 import React from "react"
 import { Link } from "gatsby";
 
+import { parseImageUrl } from 'notabase/src/utils'
+
 export default ({ data }) => {
     const { title, tags, cover_image, publish_date, desc, read_time, url, slug } = data
 
+    let coverimageURL = parseImageUrl(cover_image[0], 1000)
     return (
         <div style={{ margin: 10 }}>        
             <Link to={`subscribe/posts/${url}/`}>
+                <img 
+
+                alt={`${title} cover image`}
+
+                style={{ width: '100%' }}
+
+                src={coverimageURL}
+
+              />
             <h2>{title}</h2>
               <div style = {{color: "grey"}}>Tags: {tags && tags.join(', ')} • Published: {publish_date} • {read_time} MIN READ</div>
               
