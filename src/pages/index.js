dac38962f29f74404889ebaa9f4d8ee918d12ad5
Shreyas Prakash <shreyas314159@gmail.com>
2020-03-22 06:57:08 +0100

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,6 +55,20 @@ class HomeIndex extends React.Component {
                         <h2>Get In Touch</h2>
                         <p>“The problem of knowledge is that there are many more books on birds written by ornithologists than books on birds written by birds and books on ornithologists written by birds” - Nassim Taleb </p>
                         
+                        <div className="row">
+                            <div className="8u 12u$(small)">
+                                <form name="contact" method="post" action="#" data-netlify="true">
+                                    <div className="row uniform 50%">
+                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
+                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+                                    </div>
+                                </form>
+                                <ul className="actions">
+                                    <li><input type="submit" value="Send Message" /></li>
+                                </ul>
+                            </div>
+                            </div>
                         
                         <div className="row">
                             <div className="8u 12u$(small)">
